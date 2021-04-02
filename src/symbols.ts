c32d8ef9b501f3a9489c7f4cd47db7d074691eeb
SIGUSR97 <jeffzo01@qq.com>
2021-04-03 00:11:13 +0800

    feat: symbols for private member keys

---
+++ src/symbols.ts
@@ -0,0 +1,17 @@
+// Range symbols
+export const COMP_FUNC = Symbol('compFunc');
+export const N = Symbol('n');
+export const SIGN = Symbol('sign');
+export const LEN = Symbol('LEN');
+
+// GroupBy symbols
+export const KEY_FUNC = Symbol('keyFunc');
+export const IT = Symbol('it');
+export const TGT_KEY = Symbol('tgtkey');
+export const CURR_KEY = Symbol('currKey');
+export const CURR_VAL = Symbol('currVal');
+export const ID = Symbol('id');
+export const GROUPER = Symbol('grouper');
+
+// reversed 'magic method'
+export const REVERSED = Symbol('reversed')
