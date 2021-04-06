fde610ff4762fa6a6c98272591735e29e9b4893b
SIGUSR97 <jeffzo01@qq.com>
2021-04-06 18:20:13 +0800

    chore: changed symbols' descriptions

--- src/symbols.ts
+++ src/symbols.ts
@@ -1,22 +1,22 @@
 // Range symbols
-export const COMP_FUNC = Symbol('compFunc');
-export const N = Symbol('n');
-export const SIGN = Symbol('sign');
-export const LEN = Symbol('len');
-export const DONE = Symbol('done');
+export const COMP_FUNC = Symbol('COMP_FUNC');
+export const N = Symbol('N');
+export const SIGN = Symbol('SIGN');
+export const LEN = Symbol('LEN');
+export const DONE = Symbol('DONE');
 
 // GroupBy symbols
 export const KEY_FUNC = Symbol('keyFunc');
-export const IT = Symbol('it');
-export const TGT_KEY = Symbol('tgtkey');
-export const CURR_KEY = Symbol('currKey');
-export const CURR_VAL = Symbol('currVal');
-export const ID = Symbol('id');
-export const GROUPER = Symbol('grouper');
+export const IT = Symbol('IT');
+export const TGT_KEY = Symbol('TGT_KEY');
+export const CURR_KEY = Symbol('CURR_KEY');
+export const CURR_VAL = Symbol('CURR_VAL');
+export const ID = Symbol('ID');
+export const GROUPER = Symbol('GROUPER');
 
 // kwargs
-export const KWARGS = Symbol('kwargs');
+export const KWARGS = Symbol('KWARGS');
 
 
 // reversed 'magic method'
-export const REVERSED = Symbol('reversed')
+export const REVERSED = Symbol('REVERSED')
