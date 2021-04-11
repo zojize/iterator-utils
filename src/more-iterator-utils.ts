cdccf848cc917331fe062c32adff0823403ea079
SIGUSR97 <jeffzo01@qq.com>
2021-04-11 23:06:50 +0800

    feat: adjacentNTuples

--- src/more-iterator-utils.ts
+++ src/more-iterator-utils.ts
@@ -243,3 +243,19 @@ export function* successors<T>(first: T, succ: (x: T) => T): Generator<T> {
     yield x;
     for (;;) yield (x = succ(x));
 }
+
+// https://github.com/manim-kindergarten/manim/blob/cd19538a80b85a0f3e2469bef26137c012bb210a/manimlib/utils/iterables.py#L36
+export function adjacentNTuples<T, N extends number>(it: CanIter<T>, n: N): Generator<Tuple<T, N>> {
+    const arr = list(iter(it));
+    return zip(...imap((k) => [...arr.slice(k), ...arr.slice(0, k)], range(n))) as Generator<
+        Tuple<T, N>
+    >;
+}
+
+export const adjacent_n_tuples = adjacentNTuples;
+
+export function adjacentPairs<T>(it: CanIter<T>): Generator<[T, T]> {
+    return adjacentNTuples(it, 2);
+}
+
+export const adjacent_pairs = adjacentPairs;
