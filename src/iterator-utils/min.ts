3c0b7c420e929eec8951173f43b56b23d511b878
SIGUSR97 <jeffzo01@qq.com>
2021-06-02 23:19:07 +0800

    style: ordered imports

--- src/iterator-utils/min.ts
+++ src/iterator-utils/min.ts
@@ -1,6 +1,6 @@
+import { ireduce } from './ireduce';
 import type { NumberPredicateFunction } from '../internal/types';
 import { numberIdentity } from '../internal/utils';
-import { ireduce } from './ireduce';
 
 export function min<T>(it: Iterable<T>, key: NumberPredicateFunction<T> = numberIdentity): T {
     return ireduce(it, (a, b) => (key(a) < key(b) ? a : b));
